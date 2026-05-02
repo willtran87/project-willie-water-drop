3f3228c99b7ed75f6bd6aaa94f0cc2094ab2c6b4
Will Tran <willtran87@gmail.com>
2026-05-02 11:29:17 -0500

    feat: add Phaser <-> React event bridge

---
+++ src/game/events.ts
@@ -0,0 +1,18 @@
+import { EventEmitter } from 'phaser'
+
+export const GameEvents = {
+  // Phaser -> React
+  SCORE_CHANGED: 'score-changed',
+  OBJECTIVE_REACHED: 'objective-reached',
+  PLAYER_DIED: 'player-died',
+  GAME_STARTED: 'game-started',
+
+  // React -> Phaser
+  START_LEVEL: 'start-level',
+  RESTART: 'restart',
+  TRIVIA_ANSWERED: 'trivia-answered',
+  PAUSE: 'pause',
+  RESUME: 'resume',
+} as const
+
+export const gameEventEmitter = new EventEmitter()
