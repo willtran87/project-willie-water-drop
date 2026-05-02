25f05fe2e12d0ad8cc5a08300a3ea999861d0f65
Will Tran <willtran87@gmail.com>
2026-05-02 15:58:59 -0500

    fix: use absolute paths for asset loading to work with client-side routing

--- src/game/scenes/PreloadScene.ts
+++ src/game/scenes/PreloadScene.ts
@@ -7,57 +7,57 @@ export class PreloadScene extends Phaser.Scene {
 
   preload() {
     // Audio
-    this.load.audio('jump', 'assets/audio/jump.m4a')
-    this.load.audio('hit', 'assets/audio/hit.m4a')
-    this.load.audio('reach', 'assets/audio/reach.m4a')
+    this.load.audio('jump', '/assets/audio/jump.m4a')
+    this.load.audio('hit', '/assets/audio/hit.m4a')
+    this.load.audio('reach', '/assets/audio/reach.m4a')
 
     // Willie spritesheets
-    this.load.spritesheet('willie', 'assets/sprites/willie-run.png', {
+    this.load.spritesheet('willie', '/assets/sprites/willie-run.png', {
       frameWidth: 88, frameHeight: 94,
     })
 
     // Willie static images
-    this.load.image('willie-idle', 'assets/sprites/willie-idle.png')
-    this.load.image('willie-hurt', 'assets/sprites/willie-hurt.png')
-    this.load.image('willie-cool', 'assets/sprites/willie-cool.png')
-    this.load.image('willie-intro', 'assets/sprites/willie-intro.png')
-    this.load.image('willie-sobbing', 'assets/sprites/willie-sobbing.png')
-    this.load.image('willie-cool-and-hydrated', 'assets/sprites/cool-and-hydrated.png')
-    this.load.image('willie-professor', 'assets/sprites/willie-professor.png')
+    this.load.image('willie-idle', '/assets/sprites/willie-idle.png')
+    this.load.image('willie-hurt', '/assets/sprites/willie-hurt.png')
+    this.load.image('willie-cool', '/assets/sprites/willie-cool.png')
+    this.load.image('willie-intro', '/assets/sprites/willie-intro.png')
+    this.load.image('willie-sobbing', '/assets/sprites/willie-sobbing.png')
+    this.load.image('willie-cool-and-hydrated', '/assets/sprites/cool-and-hydrated.png')
+    this.load.image('willie-professor', '/assets/sprites/willie-professor.png')
 
     // Obstacle images (hydrants)
-    this.load.image('obsticle-1', 'assets/sprites/hydrant_small_1.png')
-    this.load.image('obsticle-2', 'assets/sprites/hydrant_small_2.png')
-    this.load.image('obsticle-3', 'assets/sprites/hydrant_small_3.png')
-    this.load.image('obsticle-4', 'assets/sprites/hydrant_big_1.png')
-    this.load.image('obsticle-5', 'assets/sprites/hydrant_big_2.png')
-    this.load.image('obsticle-6', 'assets/sprites/hydrant_big_3.png')
+    this.load.image('obsticle-1', '/assets/sprites/hydrant_small_1.png')
+    this.load.image('obsticle-2', '/assets/sprites/hydrant_small_2.png')
+    this.load.image('obsticle-3', '/assets/sprites/hydrant_small_3.png')
+    this.load.image('obsticle-4', '/assets/sprites/hydrant_big_1.png')
+    this.load.image('obsticle-5', '/assets/sprites/hydrant_big_2.png')
+    this.load.image('obsticle-6', '/assets/sprites/hydrant_big_3.png')
 
     // Animated obstacle spritesheets
-    this.load.spritesheet('water-meter', 'assets/sprites/water-meter.png', { frameWidth: 90, frameHeight: 90 })
-    this.load.spritesheet('root-ball', 'assets/sprites/root-ball.png', { frameWidth: 92, frameHeight: 77 })
-    this.load.spritesheet('vroom-vroom_small', 'assets/sprites/vroom-vroom_small.png', { frameWidth: 200, frameHeight: 100 })
-    this.load.spritesheet('vroom-vroom_big', 'assets/sprites/vroom-vroom_big.png', { frameWidth: 400, frameHeight: 250 })
-    this.load.spritesheet('jet-small', 'assets/sprites/jet-small.png', { frameWidth: 200, frameHeight: 100 })
-    this.load.spritesheet('jet-big', 'assets/sprites/jet-big.png', { frameWidth: 400, frameHeight: 250 })
-    this.load.spritesheet('meter-van', 'assets/sprites/meter-van.png', { frameWidth: 125, frameHeight: 80 })
-    this.load.spritesheet('utility-truck', 'assets/sprites/utility-truck.png', { frameWidth: 150, frameHeight: 80 })
+    this.load.spritesheet('water-meter', '/assets/sprites/water-meter.png', { frameWidth: 90, frameHeight: 90 })
+    this.load.spritesheet('root-ball', '/assets/sprites/root-ball.png', { frameWidth: 92, frameHeight: 77 })
+    this.load.spritesheet('vroom-vroom_small', '/assets/sprites/vroom-vroom_small.png', { frameWidth: 200, frameHeight: 100 })
+    this.load.spritesheet('vroom-vroom_big', '/assets/sprites/vroom-vroom_big.png', { frameWidth: 400, frameHeight: 250 })
+    this.load.spritesheet('jet-small', '/assets/sprites/jet-small.png', { frameWidth: 200, frameHeight: 100 })
+    this.load.spritesheet('jet-big', '/assets/sprites/jet-big.png', { frameWidth: 400, frameHeight: 250 })
+    this.load.spritesheet('meter-van', '/assets/sprites/meter-van.png', { frameWidth: 125, frameHeight: 80 })
+    this.load.spritesheet('utility-truck', '/assets/sprites/utility-truck.png', { frameWidth: 150, frameHeight: 80 })
 
     // Environment
-    this.load.image('ground', 'assets/environment/ground.png')
-    this.load.image('cloud', 'assets/environment/cloud.png')
-    this.load.image('tree', 'assets/environment/tree.png')
-    this.load.image('bench', 'assets/environment/bench.png')
-    this.load.image('water-fountain', 'assets/environment/water-fountain.png')
-    this.load.image('water-tower-1', 'assets/environment/water-tower-1.png')
-    this.load.image('water-tower-2', 'assets/environment/water-tower-2.png')
+    this.load.image('ground', '/assets/environment/ground.png')
+    this.load.image('cloud', '/assets/environment/cloud.png')
+    this.load.image('tree', '/assets/environment/tree.png')
+    this.load.image('bench', '/assets/environment/bench.png')
+    this.load.image('water-fountain', '/assets/environment/water-fountain.png')
+    this.load.image('water-tower-1', '/assets/environment/water-tower-1.png')
+    this.load.image('water-tower-2', '/assets/environment/water-tower-2.png')
 
     // UI images
-    this.load.image('game-over', 'assets/ui/game-over.png')
-    this.load.image('restart', 'assets/ui/restart.png')
-    this.load.image('incorrect', 'assets/ui/incorrect.png')
-    this.load.image('times-up', 'assets/ui/times-up.png')
-    this.load.image('congratulations', 'assets/ui/congratulations.png')
+    this.load.image('game-over', '/assets/ui/game-over.png')
+    this.load.image('restart', '/assets/ui/restart.png')
+    this.load.image('incorrect', '/assets/ui/incorrect.png')
+    this.load.image('times-up', '/assets/ui/times-up.png')
+    this.load.image('congratulations', '/assets/ui/congratulations.png')
   }
 
   create() {
